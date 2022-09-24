f3f6020724853b3d2734870b435fc523c94059cf
Masa <mssa.massan@gmail.com>
2022-09-25 04:08:48 +0900

    フォントの読み込みの方法の変更

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -115,6 +115,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      {
+        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap',
+        rel: 'stylesheet',
+      },
     ],
   },
 
