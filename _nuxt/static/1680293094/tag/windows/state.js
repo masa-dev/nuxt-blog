7bcb9c0f319b30b08ff8f826f4f7cd8accddbd01
masa-dev <masa-dev@users.noreply.github.com>
2023-03-31 20:05:01 +0000

    deploy: 83914ed68a0e4ac464e1cd3621cd35c089bfae8a

---
+++ _nuxt/static/1680293094/tag/windows/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680293094",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftag\u002Fwindows",config:{apiUrl:"https:\u002F\u002Fnuxt-blog.cdn.newt.so\u002Fv1\u002Fblog",apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
