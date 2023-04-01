2de50c06a157bcfb7fbbb375a04deed3856f2f7b
masa-dev <masa-dev@users.noreply.github.com>
2023-04-01 14:12:25 +0000

    deploy: 4c26c34a147734ae2917c08794fe268dcf7b3691

---
+++ _nuxt/static/1680358337/post/63d6a6932772b5781daab492/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680358337",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpost\u002F63d6a6932772b5781daab492",config:{apiUrl:"https:\u002F\u002Fnuxt-blog.cdn.newt.so\u002Fv1\u002Fblog",apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
