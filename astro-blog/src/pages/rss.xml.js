10085fe0b01704e51b6fa3e74812407cbd3c50b8
Masa <mssa.massan@gmail.com>
2024-09-22 12:25:32 +0900

    format: prettierでのフォーマット適用

--- astro-blog/src/pages/rss.xml.js
+++ astro-blog/src/pages/rss.xml.js
@@ -1,29 +1,29 @@
-import rss from '@astrojs/rss';
-import { fetchPosts } from '../lib/newtClient'
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { fetchPosts } from "../lib/newtClient";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const postRes = await fetchPosts({ limit: 100, skip: 0 });
-	const noteRes = await fetchPosts({ limit: 100, skip: 0 });
-	const postItems = postRes.items.map((post) => ({
-		title: post.title,
-		description: post.description,
-		content: post.body,
-		pubDate: post._sys.createdAt,
-		link: `/post/${post._id}`,
-	}));
-	const noteItems = noteRes.items.map((note) => ({
-		title: note.title,
-		description: note.description,
-		content: note.body,
-		pubDate: note._sys.createdAt,
-		link: `/note/${note._id}`,
-	}));
+  const postRes = await fetchPosts({ limit: 100, skip: 0 });
+  const noteRes = await fetchPosts({ limit: 100, skip: 0 });
+  const postItems = postRes.items.map((post) => ({
+    title: post.title,
+    description: post.description,
+    content: post.body,
+    pubDate: post._sys.createdAt,
+    link: `/post/${post._id}`,
+  }));
+  const noteItems = noteRes.items.map((note) => ({
+    title: note.title,
+    description: note.description,
+    content: note.body,
+    pubDate: note._sys.createdAt,
+    link: `/note/${note._id}`,
+  }));
 
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: [...postItems, ...noteItems],
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: [...postItems, ...noteItems],
+  });
 }
--- astro-blog/src/util/setHeadingId.ts
+++ astro-blog/src/util/setHeadingId.ts
@@ -1,8 +1,8 @@
 const setHeadingId = (body: string) => {
-    let headingCount = 1;
-    return body.replace(/<h([1-3]{1})>/g, (_, headingNum: string) => {
-        return `<h${headingNum} id="content-h-${headingCount++}">`;
-    });
-}
+  let headingCount = 1;
+  return body.replace(/<h([1-3]{1})>/g, (_, headingNum: string) => {
+    return `<h${headingNum} id="content-h-${headingCount++}">`;
+  });
+};
 
 export default setHeadingId;
