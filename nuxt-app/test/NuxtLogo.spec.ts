2e96fe16cc3abf4d0fc7d27a98421bc4ad8a75e5
Masa <mssa.massan@gmail.com>
2026-06-11 22:50:57 +0900

    fix: 旧nuxtアプリケーションのソースコードを削除

--- nuxt-app/test/NuxtLogo.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { mount } from '@vue/test-utils'
-
-// describe('NuxtLogo', () => {
-//   test('is a Vue instance', () => {
-//      const wrapper = mount(NuxtLogo)
-//      expect(wrapper.vm).toBeTruthy()
-//   })
-// })
